c6490d29bfcdcb72be7c01f7150d91de00599ec8
Quesses <quesses1@gmail.com>
2022-09-07 18:32:10 +0000

    shell

--- main.h
+++ main.h
@@ -1,14 +1,80 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdio.h>
 #include <stddef.h>
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* buffers to read/write */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* command chains */
+#define CMD_NORM        0
+#define CMD_OR          1
+#define CMD_AND         2
+#define CMD_CHAIN       3
+
+/* convert_number() */
+#define CONVERT_LOWERCASE       1
+#define CONVERT_UNSIGNED        2
+
+/* 1 if using system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE       ".simple_shell_history"
+#define HIST_MAX        4096
+
+extern char **environ;
 
 
+/**
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
+ */
+typedef struct liststr
+{
+        int num;
+        char *str;
+        struct liststr *next;
+} list_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+        0, 0, 0}
+
+/**
+ *struct builtin - contains a builtin string and related function
+ *@type: the builtin command flag
+ *@func: the function
+ */
+typedef struct builtin
+{
+        char *type;
+        int (*func)(info_t *);
+} builtin_table;
 
 
+/* toem_shloop.c */
+int hsh(info_t *, char **);
+int find_builtin(info_t *);
+void find_cmd(info_t *);
+void fork_cmd(info_t *);
+
+/* toem_parser.c */
+int is_cmd(info_t *, char *);
 
 #endif /* _MAIN_H_ */
